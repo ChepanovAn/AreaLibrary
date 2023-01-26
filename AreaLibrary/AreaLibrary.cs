34d782d5876a1c9d016a162be900af183c9f36a5
Анатолий <Chepanov199287@gmail.com>
2023-01-26 11:02:03 +0300

    Initial

---
+++ AreaLibrary/AreaLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaLibrary
+{
+    public static class AreaLibrary
+    {
+        public static double RectangleArea (double firstvalue, double secondvalue, double thirdvalue)
+        {
+            double p = (firstvalue + secondvalue + thirdvalue) / 2;
+            return Math.Sqrt(p * (p - firstvalue) * (p - secondvalue) * (p - thirdvalue));
+        }
+        public static double CircleArea(double firstvalue)
+        {
+            return 3.14 * firstvalue;
+        }
+    }
+}
